1a9328e46fb188b31f1faff592526f50a4f68c09
purushothamreddyaccionlabs <121781414+purushothamreddyaccionlabs@users.noreply.github.com>
2023-01-05 15:40:50 +0530

    Add files via upload

---
+++ Arraypairs1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Example 2:
+
+Input: nums = [1, 1, 1, 1]
+Output: 6
+Explanation: Each pair in the array are good.*/
+
+using System;
+    class Arraypairs1
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 1, 1, 1 };
+        int output = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            for(int j=0; j< arr.Length; j++)
+            {
+                if (arr[i] == arr[j] && i<j)
+                {
+                    output++;
+                }
+            }
+        }
+        Console.WriteLine(output);
+    }
+}
